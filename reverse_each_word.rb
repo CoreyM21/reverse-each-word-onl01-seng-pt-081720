e6374201c2c09dbe560332a74924094bfdede29d
Corey Martin <coreyjmartin2@gmail.com>
2020-08-26 20:46:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,7 @@ end
 def reverse_each_word(sentence)
   array_sentence = sentence.split(" ")
   nusentence = []
-  array_sentence.each do |string|
+  array_sentence.map do |string|
   nusentence << string.reverse
   end
   nusentence.join(" ")
